20d83db892a1f311c45516b3c8bfe1a6b567f476
YIPG <around.mr.child@gmail.com>
2019-09-15 14:03:36 +0900

    ぴょん

---
+++ Rust/adder/src/lib.rs
@@ -0,0 +1,30 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+}
+
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
