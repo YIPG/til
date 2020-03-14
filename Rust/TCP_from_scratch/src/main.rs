c12d538273901e309f3b8b3642a116d2386dc533
yipg <around.mr.child@gmail.com>
2020-03-14 10:50:48 +0900

    a

--- Rust/TCP_from_scratch/src/main.rs
+++ Rust/TCP_from_scratch/src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 fn main() -> io::Result<()> {
     let nic = tun_tap_mac::Iface::new("tun0", tun_tap_mac::Mode::Tun)?;
     let mut buf = [0u8; 1504];
-    let nbytes = nic.recv(&mut buf[..])?;
+    let nbytes = nic.recv(&mut buf[..]).expect("failed at nit recv");
     eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
     Ok(())
 }
